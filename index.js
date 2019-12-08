dfbe365b154be55ad494b12a37b01c344f4ca30e
niteshnitu <niteshgoud7366@gmail.com>
2019-12-09 00:43:37 +0530

    After removing payment methods

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys')
 const authRoutes = require('./routes/authRoutes');
-const billingRoutes = require('./routes/billingRoutes');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const bodyParser = require('body-parser');
@@ -23,8 +22,6 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 authRoutes(app);
-billingRoutes(app);
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
