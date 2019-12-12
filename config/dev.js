2d21eca2a8b39e3ad0473c074653515a7804ba24
niteshnitu <niteshgoud7366@gmail.com>
2019-12-12 22:35:22 +0530

    Emailer to Heroku

---
+++ config/dev.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+    googleClientID: "365780831439-2e9f3ve3aeu5gpeci377hvd3815ktrqp.apps.googleusercontent.com",
+    googleClientSecret: "6pXS0_LKu6K2_i8oJ-sOOqTg",
+    mongoURI: "mongodb+srv://niteshgoud:Nitu12..@cluster0-yllxw.mongodb.net/test?retryWrites=true&w=majority",
+    cookieKeys: "njcwjewjekvfjewnffnwfjnanejvncdknvkasueifhuein",
+    stripePublisableKey: "pk_test_CLhs5ICQRYef0JjJdKuiAvnA00xgtemvZK",
+    stripeSecretKey: "sk_test_1bb74fvCbB6O5VUF6KPL98eC00AYZ6v2rp",
+    sendGridKey: "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",
+    redirectDomain: "http://localhost:3000"
+};
